cbd295312f7f49033432af6705cda1807101c1f5
Yoshiori SHOJI <yoshiori@gmail.com>
2012-04-27 17:59:14 +0900

    test 追加

---
+++ bf_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+import unittest
+import bf
+
+from itertools import ifilterfalse,count
+
+class MockStdout(object):
+    text = ''
+    def write(self,str):
+        self.text += str
+    
+
+class TestBfOneliner(unittest.TestCase):
+
+    def setUp(self):
+        self.stdout = MockStdout()
+        self.default_stdout = sys.stdout
+        sys.stdout = self.stdout
+
+    def tearDown(self):
+        sys.stdout = self.default_stdout
+    
+    def testBf(self):
+        eval(bf.compile('>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.'))
+        self.assertEqual(self.stdout.text, 'Hello World!\n')
+
+if __name__ == '__main__':
+    unittest.main()
